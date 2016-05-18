2f59fcbe307182555926ef82055644ff8bf096ed
jhasmanyQuiroz <jhasmany.quiroz.olivera@gmail.com>
2016-05-18 18:48:39 -0400

    Class that represents to Lead page

---
+++ src/main/java/com/jalasoft/testing/pages/LeadHome.java
@@ -0,0 +1,11 @@
+package com.jalasoft.testing.pages;
+
+/**
+ * Created by Jhasmany Quiroz on 5/18/2016.
+ */
+public class LeadHome extends AbstractBasePage {
+
+    public RelatedList goToRelatedList() {
+        return new RelatedList();
+    }
+}
