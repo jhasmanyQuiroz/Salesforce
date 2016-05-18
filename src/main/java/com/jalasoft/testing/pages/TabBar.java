48b8f01ac18dfda3f415380bca277a3b45728c88
jhasmanyQuiroz <jhasmany.quiroz.olivera@gmail.com>
2016-05-18 18:51:13 -0400

    Add the method to does click in the Leads Tab

--- src/main/java/com/jalasoft/testing/pages/TabBar.java
+++ src/main/java/com/jalasoft/testing/pages/TabBar.java
@@ -12,10 +12,20 @@ public class TabBar extends AbstractBasePage {
     @FindBy(css = "#Campaign_Tab > a")
     private WebElement campaignTab;
 
+    //@FindBy(css = "")
+    //a[@title='Leads Tab - Selected']")); driver.findElement(By.xpath("//a[@title='Leads Tab']"));
+    @FindBy(xpath = "//a[@title='Leads Tab']")
+    private WebElement leadTab;
+
     public CampaignHome clickCampaignsTab() {
         wait.until(ExpectedConditions.elementToBeClickable(campaignTab));
         campaignTab.click();
         return new CampaignHome();
     }
 
+    public LeadHome clickLeadsTab() {
+        wait.until(ExpectedConditions.elementToBeClickable(leadTab));
+        leadTab.click();
+        return new LeadHome();
+    }
 }
